9627ac40f25b053c6db61cc28bc8a4dd394f9c56
YGOMod <admir_zuzic@hotmail.com>
2024-09-11 19:46:00 +0200

    fix

--- app/src/main/java/kr/co/iefriends/pcsx2/BaseActivity.java
+++ app/src/main/java/kr/co/iefriends/pcsx2/BaseActivity.java
@@ -1,7 +1,6 @@
 package kr.co.iefriends.pcsx2;
 
 import android.os.Bundle;
-import android.os.Message;
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
