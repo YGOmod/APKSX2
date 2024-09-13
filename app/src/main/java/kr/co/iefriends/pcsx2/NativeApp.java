9f392c91d54a622e6205563db255caf9f3cced58
YGOMod <admir_zuzic@hotmail.com>
2024-09-13 18:10:13 +0200

    fix

--- app/src/main/java/kr/co/iefriends/pcsx2/NativeApp.java
+++ app/src/main/java/kr/co/iefriends/pcsx2/NativeApp.java
@@ -11,87 +11,87 @@ import java.io.IOException;
 import java.lang.ref.WeakReference;
 
 public class NativeApp {
-	static {
-		try {
-			System.loadLibrary("emucore");
-			hasNoNativeBinary = false;
-		} catch (UnsatisfiedLinkError e) {
-			hasNoNativeBinary = true;
-		}
-	}
+    static {
+        try {
+            System.loadLibrary("emucore");
+            hasNoNativeBinary = false;
+        } catch (UnsatisfiedLinkError e) {
+            hasNoNativeBinary = true;
+        }
+    }
 
-	public static boolean hasNoNativeBinary;
-
-
-	protected static WeakReference<Context> mContext;
-	public static Context getContext() {
-		return mContext.get();
-	}
-
-	public static void initializeOnce(Context context) {
-		mContext = new WeakReference<>(context);
-		File externalFilesDir = context.getExternalFilesDir(null);
-		if (externalFilesDir == null) {
-			externalFilesDir = context.getDataDir();
-		}
-		initialize(externalFilesDir.getAbsolutePath(), android.os.Build.VERSION.SDK_INT);
-	}
-
-	public static int openContentUri(String uriString) {
-	BaseActivity currentActivity = MainApplication.getCurrentActivity();
-	if (currentActivity != null) {
-		try {
-			ParcelFileDescriptor fileDescriptor = currentActivity
-					.getApplication()
-					.getContentResolver()
-					.openFileDescriptor(Uri.parse(uriString), "r");
-			if (fileDescriptor != null) {
-				return fileDescriptor.detachFd();
-			}
-			return -1;
-		} catch (IOException ignored) {
-			return -1;
-		}
-	}
-	return -1;
-	}
+    public static boolean hasNoNativeBinary;
 
 
-	public static native void initialize(String path, int apiVer);
-	public static native String getGameTitle(String path);
-	public static native String getGameSerial();
-	public static native float getFPS();
+    protected static WeakReference<Context> mContext;
+    public static Context getContext() {
+        return mContext.get();
+    }
 
-	public static native String getPauseGameTitle();
-	public static native String getPauseGameSerial();
+    public static void initializeOnce(Context context) {
+        mContext = new WeakReference<>(context);
+        File externalFilesDir = context.getExternalFilesDir(null);
+        if (externalFilesDir == null) {
+            externalFilesDir = context.getDataDir();
+        }
+        initialize(externalFilesDir.getAbsolutePath(), android.os.Build.VERSION.SDK_INT);
+    }
 
-	public static native void setPadVibration(boolean isonoff);
-	public static native void setPadButton(int index, int range, boolean iskeypressed);
-	public static native void resetKeyStatus();
+    public static int openContentUri(String uriString) {
+    BaseActivity currentActivity = MainApplication.getCurrentActivity();
+    if (currentActivity != null) {
+        try {
+            ParcelFileDescriptor fileDescriptor = currentActivity
+                    .getApplication()
+                    .getContentResolver()
+                    .openFileDescriptor(Uri.parse(uriString), "r");
+            if (fileDescriptor != null) {
+                return fileDescriptor.detachFd();
+            }
+            return -1;
+        } catch (IOException ignored) {
+            return -1;
+        }
+    }
+    return -1;
+    }
 
-	public static native void setAspectRatio(int type);
-	public static native void speedhackLimitermode(int value);
-	public static native void speedhackEecyclerate(int value);
-	public static native void speedhackEecycleskip(int value);
 
-	public static native void renderUpscalemultiplier(float value);
-	public static native void renderMipmap(int value);
-	public static native void renderHalfpixeloffset(int value);
-	public static native void renderGpu(int value);
-	public static native void renderPreloading(int value);
+    public static native void initialize(String path, int apiVer);
+    public static native String getGameTitle(String path);
+    public static native String getGameSerial();
+    public static native float getFPS();
 
-	public static native void onNativeSurfaceCreated();
-	public static native void onNativeSurfaceChanged(Surface surface, int w, int h);
-	public static native void onNativeSurfaceDestroyed();
+    public static native String getPauseGameTitle();
+    public static native String getPauseGameSerial();
 
-	public static native boolean runVMThread(String path);
+    public static native void setPadVibration(boolean isonoff);
+    public static native void setPadButton(int index, int range, boolean iskeypressed);
+    public static native void resetKeyStatus();
 
-	public static native void pause();
-	public static native void resume();
-	public static native void shutdown();
+    public static native void setAspectRatio(int type);
+    public static native void speedhackLimitermode(int value);
+    public static native void speedhackEecyclerate(int value);
+    public static native void speedhackEecycleskip(int value);
 
-	public static native boolean saveStateToSlot(int slot);
-	public static native boolean loadStateFromSlot(int slot);
-	public static native String getGamePathSlot(int slot);
-	public static native byte[] getImageSlot(int slot);
+    public static native void renderUpscalemultiplier(float value);
+    public static native void renderMipmap(int value);
+    public static native void renderHalfpixeloffset(int value);
+    public static native void renderGpu(int value);
+    public static native void renderPreloading(int value);
+
+    public static native void onNativeSurfaceCreated();
+    public static native void onNativeSurfaceChanged(Surface surface, int w, int h);
+    public static native void onNativeSurfaceDestroyed();
+
+    public static native boolean runVMThread(String path);
+
+    public static native void pause();
+    public static native void resume();
+    public static native void shutdown();
+
+    public static native boolean saveStateToSlot(int slot);
+    public static native boolean loadStateFromSlot(int slot);
+    public static native String getGamePathSlot(int slot);
+    public static native byte[] getImageSlot(int slot);
 }
